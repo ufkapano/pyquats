dd45ff7c71ef81e0d9ed8bb7c511b4362abdcae1
Andrzej Kapanowski <ufkapano@gmail.com>
2015-02-11 10:37:43 +0100

    qmath.sinh() and qmath.cosh() added.

--- src/qmath.py
+++ src/qmath.py
@@ -25,6 +25,7 @@ def exp(x):
         result = result * math.exp(x.q[0])
         return result
 
+
 def sin(x):
     """Return the sine of x."""
     if isinstance(x, (int, long, float)):
@@ -46,11 +47,39 @@ def cos(x):
         return Quat(math.cos(x))
     elif isinstance(x, complex):
         return Quat(math.cos(x.real) * math.cosh(x.imag), 
-                    -math.sin(x.real) * math.sinh(x.imag))
+                   -math.sin(x.real) * math.sinh(x.imag))
     else:   # isinstance(x, Quat)
         # cos(q)=(exp(qJ)+exp(-qJ))/2
         quat = x * Quat(0, 1)
         result = exp(quat) + exp(-quat)
+        result = result * Quat(0.5)
+        return result
+
+
+def sinh(x):
+    """Return the hyperbolic sine of x."""
+    if isinstance(x, (int, long, float)):
+        return Quat(math.sinh(x))
+    elif isinstance(x, complex):
+        return Quat(math.sinh(x.real) * math.cos(x.imag), 
+                    math.cosh(x.real) * math.sin(x.imag))
+    else:   # isinstance(x, Quat)
+        # sin(q)=(exp(q)-exp(-q))/2
+        result = exp(x) - exp(-x)
+        result = result * Quat(0.5)
+        return result
+
+
+def cosh(x):
+    """Return the hyperbolic cosine of x."""
+    if isinstance(x, (int, long, float)):
+        return Quat(math.cosh(x))
+    elif isinstance(x, complex):
+        return Quat(math.cosh(x.real) * math.cos(x.imag), 
+                    math.sinh(x.real) * math.sin(x.imag))
+    else:   # isinstance(x, Quat)
+        # cos(q)=(exp(q)+exp(-q))/2
+        result = exp(x) + exp(-x)
         result = result * Quat(0.5)
         return result
 
--- src/test_qmath.py
+++ src/test_qmath.py
@@ -10,42 +10,61 @@ from quats import Quat
 class TestQuatMath(unittest.TestCase):
 
     def setUp(self):
+        self.f1 = 3.0
+        self.f2 = 4.0
         self.c1 = 1J
         self.c2 = 1 + 2J
 
     def test_exp(self):
-        self.assertAlmostEqual(qmath.exp(2), math.exp(2))
-        self.assertAlmostEqual(qmath.exp(3.0), math.exp(3.0))
+        self.assertAlmostEqual(qmath.exp(self.f1), math.exp(self.f1))
         self.assertAlmostEqual(qmath.exp(self.c1), cmath.exp(self.c1))
         self.assertAlmostEqual(qmath.exp(self.c2), cmath.exp(self.c2))
-        self.assertAlmostEqual(qmath.exp(Quat(2)), math.exp(2))
+        self.assertAlmostEqual(qmath.exp(Quat(self.f1)), math.exp(self.f1))
         self.assertAlmostEqual(qmath.exp(Quat(0, 3)), cmath.exp(3J))
         self.assertAlmostEqual(qmath.exp(Quat(4, 5)), cmath.exp(4 + 5J))
         self.assertAlmostEqual(qmath.exp(Quat(0, math.pi)), -1.0)
         self.assertAlmostEqual(qmath.exp(Quat(0, 0, math.pi)), -1.0)
         self.assertAlmostEqual(qmath.exp(Quat(0, 0, 0, math.pi)), -1.0)
-        self.assertAlmostEqual(qmath.exp(Quat(4, 0, 3)),
-            math.exp(4) * Quat(math.cos(3), 0, math.sin(3)))
-        self.assertAlmostEqual(qmath.exp(Quat(2, 0, 0, 3)),
-            math.exp(2) * Quat(math.cos(3), 0, 0, math.sin(3)))
+        self.assertAlmostEqual(qmath.exp(Quat(self.f1, 0, self.f2)),
+            math.exp(self.f1) * Quat(math.cos(self.f2), 0, math.sin(self.f2)))
+        self.assertAlmostEqual(qmath.exp(Quat(self.f1, 0, 0, self.f2)),
+            math.exp(self.f1) * Quat(math.cos(self.f2), 0, 0, math.sin(self.f2)))
 
     def test_sin(self):
-        self.assertAlmostEqual(qmath.sin(2), math.sin(2))
+        self.assertAlmostEqual(qmath.sin(self.f1), math.sin(self.f1))
         self.assertAlmostEqual(qmath.sin(self.c1), cmath.sin(self.c1))
         self.assertAlmostEqual(qmath.sin(self.c2), cmath.sin(self.c2))
-        self.assertAlmostEqual(qmath.sin(Quat(2)), math.sin(2))
+        self.assertAlmostEqual(qmath.sin(Quat(self.f1)), math.sin(self.f1))
         self.assertAlmostEqual(qmath.sin(Quat(0, 3)), cmath.sin(3J))
         self.assertAlmostEqual(qmath.sin(Quat(4, 5)), cmath.sin(4 + 5J))
-        self.assertAlmostEqual(qmath.sin(Quat(0, 3)), Quat(0, math.sinh(3)))
+        self.assertAlmostEqual(qmath.sin(Quat(0, self.f1)), Quat(0, math.sinh(self.f1)))
 
     def test_cos(self):
-        self.assertAlmostEqual(qmath.cos(2), math.cos(2))
+        self.assertAlmostEqual(qmath.cos(self.f1), math.cos(self.f1))
         self.assertAlmostEqual(qmath.cos(self.c1), cmath.cos(self.c1))
         self.assertAlmostEqual(qmath.cos(self.c2), cmath.cos(self.c2))
-        self.assertAlmostEqual(qmath.cos(Quat(2)), math.cos(2))
+        self.assertAlmostEqual(qmath.cos(Quat(self.f1)), math.cos(self.f1))
         self.assertAlmostEqual(qmath.cos(Quat(0, 3)), cmath.cos(3J))
         self.assertAlmostEqual(qmath.cos(Quat(4, 5)), cmath.cos(4 + 5J))
-        self.assertAlmostEqual(qmath.cos(Quat(0, 3)), Quat(math.cosh(3)))
+        self.assertAlmostEqual(qmath.cos(Quat(0, self.f1)), Quat(math.cosh(self.f1)))
+
+    def test_sinh(self):
+        self.assertAlmostEqual(qmath.sinh(self.f1), math.sinh(self.f1))
+        self.assertAlmostEqual(qmath.sinh(self.c1), cmath.sinh(self.c1))
+        self.assertAlmostEqual(qmath.sinh(self.c2), cmath.sinh(self.c2))
+        self.assertAlmostEqual(qmath.sinh(Quat(self.f1)), math.sinh(self.f1))
+        self.assertAlmostEqual(qmath.sinh(Quat(0, 3)), cmath.sinh(3J))
+        self.assertAlmostEqual(qmath.sinh(Quat(4, 5)), cmath.sinh(4 + 5J))
+        self.assertAlmostEqual(qmath.sinh(Quat(0, self.f1)), Quat(0, math.sin(self.f1)))
+
+    def test_cosh(self):
+        self.assertAlmostEqual(qmath.cosh(self.f1), math.cosh(self.f1))
+        self.assertAlmostEqual(qmath.cosh(self.c1), cmath.cosh(self.c1))
+        self.assertAlmostEqual(qmath.cosh(self.c2), cmath.cosh(self.c2))
+        self.assertAlmostEqual(qmath.cosh(Quat(self.f1)), math.cosh(self.f1))
+        self.assertAlmostEqual(qmath.cosh(Quat(0, 3)), cmath.cosh(3J))
+        self.assertAlmostEqual(qmath.cosh(Quat(4, 5)), cmath.cosh(4 + 5J))
+        self.assertAlmostEqual(qmath.cosh(Quat(0, self.f1)), Quat(math.cos(self.f1)))
 
     def tearDown(self): pass
 
