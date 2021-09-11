736277ef4170130851fd926a779785577e5d2022
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2021-09-11 22:31:09 +0200

    random_unit_quat() added.

---
+++ pyquats/tests/test_circle.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+#
+# Testowanie rozkladu S = s1*s1 + s2*s2, kiedy s1, s2 maja rozklad
+# jednorodny w przedziale (-1,1).
+# S ma rozklad jednorodny w przedziale (0,1).
+
+n = 100
+#n = 1000
+D = dict()
+
+for i in range(-n, n+1):
+    for j in range(-n, n+1):
+        if i*i+j*j < n*n:
+            #D[i*i+j*j] = D.get(i*i+j*j, 0) +1
+            D[(i*i+j*j)*10 / (n*n)] = D.get((i*i+j*j)*10 / (n*n), 0) +1
+print(D)
+for i in range(10):
+    print(i, D.get(i,0))
+
+# EOF
---
+++ pyquats/tests/test_rotation5.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python
+#
+# Based on Vesely (1982).
+#
+# Conclusions
+# random_unit_quat() is 5 times faster that random_quat_biax(),
+# but it is unclear if the distributions are similar.
+
+import math
+import random
+import timeit
+from pyquats.quats import Quat
+from pyquats.qtools import random_quat_biax
+
+def random_pair():
+    """Return a random pair (s1, s2), where s1^2 + s2^2 < 1."""
+    s1 = random.uniform(-1, 1)
+    s2 = random.uniform(-1, 1)
+    while s1 * s1 + s2 * s2 >= 1:
+        # It is faster to change both s1 and s2 then only s2!
+        s1 = random.uniform(-1, 1)
+        s2 = random.uniform(-1, 1)
+    return s1, s2
+
+def random_unit_quat():
+    """Return a random unit quat."""
+    s1, s2 = random_pair()
+    s3, s4 = random_pair()
+    S1 = s1 * s1 + s2 * s2
+    S2 = s3 * s3 + s4 * s4
+    a = math.sqrt((1.0 - S1) / S2)
+    return Quat(s1, s2, s3 * a, s4 * a)
+
+#for i in range(5):
+#    q = random_unit_quat()
+#    print(q)
+#    print(q.is_unit())
+#    print("abs {}".format(abs(q)))
+
+N = 10000
+
+print ( "random_quat_biax()..." )
+t1 = timeit.Timer(lambda: random_quat_biax())
+print ( "{} {}".format(N, t1.timeit(N)) )
+
+print ( "random_unit_quat()..." )   # over 5 times faster
+t1 = timeit.Timer(lambda: random_unit_quat())
+print ( "{} {}".format(N, t1.timeit(N)) )
+
+
+# EOF
