2976d236ff88b1c5abdc0b2ba180fb47283b9668
Andrzej Kapanowski <andrzej.kapanowski@uj.edu.pl>
2023-01-13 10:41:09 +0100

    Testing sum() and np.sum().

--- pyquats/tests/timeit_exp.py
+++ pyquats/tests/timeit_exp.py
@@ -2,6 +2,7 @@
 #
 # Results:
 # math.exp(x) is 7x faster than np.exp(x).
+# np.sum(np.exp(L)) is 2x faster than sum(math.exp() with for loop).
 
 import random
 import timeit
@@ -12,12 +13,20 @@ N = 1000000
 
 L = [random.random()*10 for i in range(N)]
 
-print ( "Testing math.exp()..." )
+print ( "Testing sum(math.exp() with for loop)..." )
 t1 = timeit.Timer(lambda: sum(math.exp(x) for x in L))
 print ( "{} {}".format(N, t1.timeit(1)) )   # single run
 
-print ( "Testing np.exp()..." )
+print ( "Testing sum(np.exp() with for loop)..." )
 t1 = timeit.Timer(lambda: sum(np.exp(x) for x in L))
+print ( "{} {}".format(N, t1.timeit(1)) )   # single run
+
+print ( "Testing sum(np.exp(L))..." )
+t1 = timeit.Timer(lambda: sum(np.exp(L)))
+print ( "{} {}".format(N, t1.timeit(1)) )   # single run
+
+print ( "Testing np.sum(np.exp(L))..." )
+t1 = timeit.Timer(lambda: np.sum(np.exp(L))) # BEST!
 print ( "{} {}".format(N, t1.timeit(1)) )   # single run
 
 # EOF
--- pyquats/tests/timeit_sin.py
+++ pyquats/tests/timeit_sin.py
@@ -2,6 +2,7 @@
 #
 # Results:
 # math.sin(x) is 6x faster than np.sin(x).
+# np.sum(np.sin(L)) is 2x faster than sum(math.sin() with for loop).
 
 import random
 import timeit
@@ -12,12 +13,20 @@ N = 1000000
 
 L = [random.random()*10 for i in range(N)]
 
-print ( "Testing math.sin()..." )
+print ( "Testing sum(math.sin() with for loop)..." )
 t1 = timeit.Timer(lambda: sum(math.sin(x) for x in L))
 print ( "{} {}".format(N, t1.timeit(1)) )   # single run
 
-print ( "Testing np.sin()..." )
+print ( "Testing sum(np.sin() with for loop)..." )
 t1 = timeit.Timer(lambda: sum(np.sin(x) for x in L))
+print ( "{} {}".format(N, t1.timeit(1)) )   # single run
+
+print ( "Testing sum(np.sin(L))..." )
+t1 = timeit.Timer(lambda: sum(np.sin(L)))
+print ( "{} {}".format(N, t1.timeit(1)) )   # single run
+
+print ( "Testing np.sum(np.sin(L))..." )
+t1 = timeit.Timer(lambda: np.sum(np.sin(L))) # BEST!
 print ( "{} {}".format(N, t1.timeit(1)) )   # single run
 
 # EOF
